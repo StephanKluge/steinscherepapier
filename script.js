85eccd4e271083723160071e8b4079d41e5d6821
StephanKluge <114141614+StephanKluge@users.noreply.github.com>
2024-08-23 22:05:11 +0200

    testing the random math function to get 0, 1 or 2

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+// Spiel Stein Schere Papier gegen Computer. 
+// Computer returnd zufällig Stein, Schere oder Paper. mathRandom wird hier wichtig.
+// Function getComputerChoice
+
+
+// function start
+// create math random. 0-2.
+// if statemant wenn 0 dann Stein, wenn 1 dann Schere, 2 Papier
+// ergebnis in variable abspeichern
+// ergebnis returnen
+// function mit console.log abspeichern
+
+// function getComputerChoice(){
+
+// };
+
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+  }
+console.log(getRandomIntInclusive(0, 2));
