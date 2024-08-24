b2c6207b645ead7442b318fee85bbf80901cd21f
StephanKluge <114141614+StephanKluge@users.noreply.github.com>
2024-08-24 13:43:38 +0200

    computer choice with math random and if els if

---
+++ bib.js
@@ -0,0 +1,7 @@
+// Random ganze Zahl inklusive kleinste und größte
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+  }
--- script.js
+++ script.js
@@ -10,19 +10,31 @@
 // ergebnis returnen
 // function mit console.log abspeichern
 
-function getComputerChoice(){
+function getComputerChoice(){ 
     function getRandomIntInclusive(min, max) {
         const minCeiled = Math.ceil(min);
         const maxFloored = Math.floor(max);
-        return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+        return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
       }
-    if (getRandomIntInclusive(0, 2) == 0) {
-        console.log("Stein");
-    } else if (getRandomIntInclusive(0, 2) == 1){
-        console.log("Schere");
-    } else if (getRandomIntInclusive(0, 2) == 2){
-        console.log("Papier");
-    };  
+    let random = getRandomIntInclusive(0,2);
+    if (random === 0) {
+        let pcStein = "Computer: Stein";
+        console.log(pcStein);
+    } else if (random === 1){
+        let pcSchere="Compputer: Schere";
+        console.log(pcSchere);
+    } else if (random === 2){
+        let pcPapier="Computer: Papier";
+        console.log(pcPapier);
+    };
 };
 getComputerChoice();
 
+
+
+// Quick and dirty
+// let randomly = 0;
+//     do{randomly = getRandomIntInclusive(0,15);
+//         console.log(randomly);
+//     }
+//     while(randomly>2);
